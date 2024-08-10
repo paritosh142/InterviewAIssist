276d24e87aacbb52f8eda602ebfc97c663a7d7dc
Paritosh <79096636+paritosh142@users.noreply.github.com>
2024-08-10 16:23:18 +0530

    Update interviewList.tsx

--- app/dashboard/_component/interviewList.tsx
+++ app/dashboard/_component/interviewList.tsx
@@ -1,51 +1,46 @@
+"use client";
+
 import { db } from "@/utils/db";
 import { InterviewAIssist } from "@/utils/schema";
 import { useUser } from "@clerk/nextjs";
-import { desc, eq } from "drizzle-orm";
 import React, { useEffect, useState } from "react";
 import InterviewItem from "./interviewItem";
+import { desc, eq } from "drizzle-orm";
 
 type Props = {};
 
 const InterviewList = (props: Props) => {
   const { user } = useUser();
 
-  const [interviewList, setInterviewList] = useState<any[]>([]);
+  const [interviewList, setInterviewList] = useState<any>(null);
 
   useEffect(() => {
-    if (user) {
-      getInterviewList();
-    }
+    user && getInterviewList();
   }, [user]);
 
+  // @ts-ignore
   const getInterviewList = async () => {
-    try {
-      const result = await db
-        .select()
-        .from(InterviewAIssist)
-        .where(
-          // @ts-ignore
-          eq(
-            InterviewAIssist.createdBy,
-            user?.primaryEmailAddress?.emailAddress
-          )
-        )
-        .orderBy(desc(InterviewAIssist.id));
+    const result = await db
+      .select()
+      .from(InterviewAIssist)
+      .where(
+        eq(InterviewAIssist.createdBy, user?.primaryEmailAddress?.emailAddress)
+      )
+      .orderBy(desc(InterviewAIssist.id));
 
-      console.log(result);
-      setInterviewList(result);
-    } catch (error) {
-      console.error("Failed to fetch interview list:", error);
-    }
+    console.log(result);
+
+    setInterviewList(result);
   };
 
   return (
     <div>
-      <h2 className="text-2xl text-gray-200 mt-8">Previous Mock Interviews</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-        {interviewList.map((item: any, index: number) => (
-          <InterviewItem interview={item} key={index} />
-        ))}
+      <h2 className="text-2xl text-gray-200  mt-8">Previous Mock Interviews</h2>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 ">
+        {interviewList &&
+          interviewList.map((item: any, index: number) => (
+            <InterviewItem interview={item} key={index} />
+          ))}
       </div>
     </div>
   );
